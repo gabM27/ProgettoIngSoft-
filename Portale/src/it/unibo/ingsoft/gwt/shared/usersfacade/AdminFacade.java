82c34513591d381b6c4e72a9476470244f8cfad2
gabM27 <gabriele.27.magazzu@gmail.com>
2022-03-09 10:06:25 +0100

    Modifica AdminFacade

--- Portale/src/it/unibo/ingsoft/gwt/shared/usersfacade/AdminFacade.java
+++ Portale/src/it/unibo/ingsoft/gwt/shared/usersfacade/AdminFacade.java
@@ -1,5 +1,23 @@
 package it.unibo.ingsoft.gwt.shared.usersfacade;
+/*
+ * Classe facciata utilizzata dall'amministratore 
+ * del portale per interagire con le operazioni
+ * a lui consentite, senza dipendere direttamente 
+ * dalle classi di basso livello che processano le 
+ * operazioni stesse.
+ * 
+ * Un amministratore puo':
+ * 1. Creare account per studenti / docenti / 
+ * 	  segreteria utilizzando il loro indirizzo email.
+ * 
+ * 2. Aggiungere / Modificare/ Visualizzare le informazioni 
+ * 	  personali degli studenti e dei docenti.
+ *
+ */
 
-public interface AdminFacade {
-	
+public class AdminFacade {
+	// Creazione account Studente
+	protected void addStudentAccount(String email){
+		
+	}
 }
