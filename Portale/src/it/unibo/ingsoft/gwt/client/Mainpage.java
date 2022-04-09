516dd90b9ad7fd0c113480329e9cb7d3f78777c3
Roukolol <junior95@hotmail.it>
2022-04-09 18:15:50 +0200

    Fixes

--- Portale/src/it/unibo/ingsoft/gwt/client/Mainpage.java
+++ Portale/src/it/unibo/ingsoft/gwt/client/Mainpage.java
@@ -3,12 +3,14 @@ package it.unibo.ingsoft.gwt.client;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
 import it.unibo.ingsoft.gwt.client.dashboards.AdminDashboard;
 import it.unibo.ingsoft.gwt.client.dashboards.ProfessorDashboard;
 import it.unibo.ingsoft.gwt.client.dashboards.SecretaryDashboard;
 import it.unibo.ingsoft.gwt.client.dashboards.StudentDashboard;
+import it.unibo.ingsoft.gwt.client.settings.ActualSession;
 import it.unibo.ingsoft.gwt.client.settings.Singleton;
 import it.unibo.ingsoft.gwt.client.settings.adminSettings.AddAccountFormPage;
 import it.unibo.ingsoft.gwt.client.settings.adminSettings.AddDepartmentFormDashboard;
@@ -16,9 +18,9 @@ import it.unibo.ingsoft.gwt.client.settings.adminSettings.AddPersonalInfoFormPro
 import it.unibo.ingsoft.gwt.client.settings.adminSettings.AddPersonalInfoFormStudentDashboard;
 import it.unibo.ingsoft.gwt.client.settings.professorSettings.CreateChangeCourseFormProfessorDashboard;
 import it.unibo.ingsoft.gwt.client.settings.professorSettings.CreateChangeTestFormProfessorDashboard;
-import it.unibo.ingsoft.gwt.client.settings.userSettings.SearchPersonalInfo;
 import it.unibo.ingsoft.gwt.client.settings.userSettings.ViewDepartmentsList;
 import it.unibo.ingsoft.gwt.client.settings.userSettings.ViewPersonalInfo;
+import it.unibo.ingsoft.gwt.client.settings.userSettings.SearchPersonalInfo;
 import it.unibo.ingsoft.gwt.shared.Status;
 
 public class Mainpage extends Composite{
